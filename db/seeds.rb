86f59408df4e783a4d3fdb0c2f73de71ccf17c70
Student <example@example.com>
2020-01-31 09:19:56 -0800

    complete API call

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,45 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+class Seed
+
+  Park.destroy_all
+  Restaurant.destroy_all
+  Shop.destroy_all
+
+
+  def self.begin
+    seed = Seed.new
+    seed.generate_parks
+  end
+
+  def generate_parks
+    rand(3..15).times |i|
+      park = Park.create!(
+        location: Faker::Book.author,
+          description: Faker::Movie.quote
+      )
+      puts "Park #{i}: Author is #{quote.author} and quotation is '#{quote.content}'."
+    end
+  end
+end
+
+Seed.begin
+
+
+
+
+15.times do |index|
+  groups = []
+  groups.push(Group.create!(name: Faker::TvShows::BreakingBad.episode,
+    genre: Faker::Lorem.words(number: 1, supplemental: true)))
+    groups
+    rand(3..15).times do |message|
+      groups.each do |group|
+        Message.create! :author => Faker::Superhero.name, :content =>  Faker::Movie.quote,
+        :group_id => group.id
+      end
+    end
+  end
+  p "Created #{Group.count} groups"
